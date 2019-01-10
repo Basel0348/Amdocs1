f8da92743f177aa2a9793753d24fe93e50feadbe
Basel Arkia <basel0348@gmail.com>
2019-01-10 20:02:21 +0200

    fix2

--- src/main/java/com/example/Amdocs1/Amdocs1Application.java
+++ src/main/java/com/example/Amdocs1/Amdocs1Application.java
@@ -1,11 +1,14 @@
 package com.example.Amdocs1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.Amdocs1.domain.logFile;
 import com.example.Amdocs1.domain.logFileRepository;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,9 +24,7 @@ public class Amdocs1Application {
 		SpringApplication.run(Amdocs1Application.class, args);
 		logger.info("Hello Spring Boot");
 	}
-	
-	
 
-
+      
 }
 
